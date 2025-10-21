7067991489d1d3783da26479d430b8e345d2abaf
Juliana Li <julianali@dhcp-10-250-177-62.harvard.edu>
2025-10-20 21:21:10 -0400

    stuff

--- apps/src/maze/skins.js
+++ apps/src/maze/skins.js
@@ -145,7 +145,7 @@ var CONFIGS = {
     // This is required when move pegman animation is set
     movePegmanAnimationFrameNumber: 9,
     hittingWallAnimation: 'wall.gif',
-    approachingGoalAnimation: 'close_goal.png',
+    approachingGoalAnimation: 'close_goal_2.png',
     pegmanHeight: 68,
     pegmanWidth: 51,
     pegmanYOffset: -14,
@@ -179,7 +179,7 @@ var CONFIGS = {
     // This is required when move pegman animation is set
     movePegmanAnimationFrameNumber: 9,
 
-    approachingGoalAnimation: 'close_goal.png',
+    approachingGoalAnimation: 'close_goal_2.png',
     pegmanHeight: 107,
     pegmanWidth: 80,
     pegmanXOffset: -12,
